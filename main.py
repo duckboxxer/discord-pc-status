3dfada3bd12321ebdb457d51f7856bf0041b5bdb
少女。 <beehive_dev@outlook.jp>
2025-04-06 10:31:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ RPC.connect()
 
 mem = psutil.virtual_memory()
 os = platform.system()  
-x64 = platform.machine()  
+machine = platform.machine()  
 cpu = psutil.cpu_times()
 mem_used = round(mem.used / (1024 ** 3), 2)
 mem_total = round(mem.total / (1024 ** 3), 2)
@@ -17,7 +17,7 @@ mem_total = round(mem.total / (1024 ** 3), 2)
 while True:
     # RPC
     RPC.update(
-        state = f"{os} | {x64}",
+        state = f"{os} | {machine}",
         details = f"RAM | {mem_used}/{mem_total}",
         large_image = "pc",
         large_text = ".."
